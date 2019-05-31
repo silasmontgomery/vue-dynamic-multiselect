0aade21b5eff006d1d27ec20f581e6e246b90129
Paul Cooper <paul@ah-studios.com>
2019-05-31 10:42:28 +0100

    Improved focus handling
    This change improves the behaviour when using multiple multi selects on the same page. It also simplifies things.

--- src/DynamicMultiSelect.vue
+++ src/DynamicMultiSelect.vue
@@ -168,7 +168,7 @@
                 }
             },
             loseFocus: function(event) {
-                if(!event.target.classList.contains('vue-dynamic-select') && (event.target.parentElement && !event.target.parentElement.classList.contains('vue-dynamic-select'))) {
+                if(!this.$el.contains(event.target)) {
                     this.hasFocus = false;
                 }
             }
